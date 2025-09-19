1398646ba27383325c2829a338b65d77436d5ad0
Mishal Parmar <mishal@hexabrain.com>
2025-09-19 17:49:14 +0530

    Docker changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { fetcher } from "@/lib/fetcher"
 import Interactive from "@/components/Interactive"
 
 export default function HomePage() {
-  const [message, setMessage] = useState("Loading...")
+  const [message, setMessage] = useState("Loading API Data...")
 
   useEffect(() => {
     fetcher<{ message: string }>("/api/hello").then((data) => setMessage(data.message))
